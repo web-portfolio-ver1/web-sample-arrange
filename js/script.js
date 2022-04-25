523190f254ac3cb7534c5048670860f126f7fad6
fashiontechlab <fashiontechlabo@gmail.com>
2022-04-25 19:58:34 +0900

    エフェクト機能を作成

--- js/script.js
+++ js/script.js
@@ -1,21 +1,50 @@
 $(function () {
 
-  //ページ内スクロール
-  var navHeight = $(".header").outerHeight();
+    //ページ内スクロール
+    var navHeight = $(".header").outerHeight();
 
-  $('a[href^="#"]').on("click", function () {
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top - navHeight;
-    $("html, body").animate({ scrollTop: position, }, 300, "swing");
-    return false;
-  });
+    $('a[href^="#"]').on("click", function () {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? "html" : href);
+        var position = target.offset().top - navHeight;
+        $("html, body").animate({ scrollTop: position, }, 300, "swing");
+        return false;
+    });
 
-  //ページトップ
-  $("#js-page-top").on("click", function () {
-    $("body,html").animate({ scrollTop: 0, }, 300);
-    return false;
-  });
-
+    //ページトップ
+    $("#js-page-top").on("click", function () {
+        $("body,html").animate({ scrollTop: 0, }, 300);
+        return false;
+    });
 });
 
+$(function(){
+    var effect_pos = 300; // 画面下からどの位置でフェードさせるか(px)
+    var effect_move = 50; // どのぐらい要素を動かすか(px)
+    var effect_time = 800; // エフェクトの時間(ms) 1秒なら1000
+
+    // フェードする前のcssを定義
+    $('.scroll-fade').css({
+        opacity: 0,
+        transform: 'translateY('+ effect_move +'px)',
+        transition: effect_time + 'ms'
+    });
+
+    // スクロールまたはロードするたびに実行
+    $(window).on('scroll load', function(){
+        var scroll_top = $(this).scrollTop();
+        var scroll_btm = scroll_top + $(this).height();
+        effect_pos = scroll_btm - effect_pos;
+
+        // effect_posがthis_posを超えたとき、エフェクトが発動
+        $('.scroll-fade').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( effect_pos > this_pos ) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
+            }
+        });
+    });
+});
