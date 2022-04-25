ed0cfae797869bc5c055ea15bfc2f433d3502946
fashiontechlab <fashiontechlabo@gmail.com>
2022-04-25 20:55:22 +0900

    エフェクト機能の修正

--- js/script.js
+++ js/script.js
@@ -19,30 +19,24 @@ $(function () {
 });
 
 $(function(){
-
-    var effect_btm = 50; // 画面下からどの位置でフェードさせるか(px)
+    var effect_pos = 50; // 画面下からどの位置でフェードさせるか(px)
     var effect_move = 100; // どのぐらい要素を動かすか(px)
     var effect_time = 1000; // エフェクトの時間(ms) 1秒なら1000
 
-    //親要素と子要素のcssを定義
+    // フェードする前のcssを定義
     $('.scroll-fade').css({
-        opacity: 0
-    });
-    $('.scroll-fade').children().each(function(){
-        $(this).css({
-            opacity: 0,
-            transform: 'translateY('+ effect_move +'px)',
-            transition: effect_time + 'ms'
-        });
+        opacity: 0,
+        transform: 'translateY('+ effect_move +'px)',
+        transition: effect_time + 'ms'
     });
 
     // スクロールまたはロードするたびに実行
     $(window).on('scroll load', function(){
         var scroll_top = $(this).scrollTop();
         var scroll_btm = scroll_top + $(this).height();
-        var effect_pos = scroll_btm - effect_btm;
+        effect_pos = scroll_btm - effect_pos;
 
-        //エフェクトが発動したとき、子要素をずらしてフェードさせる
+        // effect_posがthis_posを超えたとき、エフェクトが発動
         $('.scroll-fade').each( function() {
             var this_pos = $(this).offset().top;
             if ( effect_pos > this_pos ) {
@@ -50,16 +44,7 @@ $(function(){
                     opacity: 1,
                     transform: 'translateY(0)'
                 });
-                $(this).children().each(function(i){
-                    $(this).delay(100 + i*200).queue(function(){
-                        $(this).css({
-                            opacity: 1,
-                            transform: 'translateY(0)'
-                        }).dequeue();
-                    });
-                });
             }
         });
     });
-
 });
