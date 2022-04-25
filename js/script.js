e3a53283836825255a3c3ec39561f6dc872b2693
fashiontechlab <fashiontechlabo@gmail.com>
2022-04-25 16:48:13 +0900

    エフェクトの取り消し

--- js/script.js
+++ js/script.js
@@ -19,22 +19,3 @@ $(function () {
 
 });
 
-function showElementAnimation() {
-
-  var works-img = document.getElementsByClassName('js-animation');
-  if(!works-img) return; // 要素がなかったら処理をキャンセル
-
-  var showTiming = window.innerHeight > 768 ? 200 : 40; // 要素が出てくるタイミングはここで調整
-  var scrollY = window.pageYOffset;
-  var windowH = window.innerHeight;
-
-  for(var i=0;i<works-img.length;i++) { var elemClientRect = works-img[i].getBoundingClientRect(); var elemY = scrollY + elemClientRect.top; if(scrollY + windowH - showTiming > elemY) {
-      works-img[i].classList.add('is-show');
-    } else if(scrollY + windowH < elemY) {
-      // 上にスクロールして再度非表示にする場合はこちらを記述
-      works-img[i].classList.remove('is-show');
-    }
-  }
-}
-showElementAnimation();
-window.addEventListener('scroll', showElementAnimation);
